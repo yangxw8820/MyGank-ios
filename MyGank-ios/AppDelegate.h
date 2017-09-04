7e60d5bc7e2b3ee1f1f06274f0c2de4e441ec141
yangxw8820 <yangxw8820@users.noreply.github.com>
2017-09-04 15:24:49 +0800

    init
    init commit

---
+++ MyGank-ios/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MyGank-ios
+//
+//  Created by ycl on 2017/9/4.
+//  Copyright © 2017年 ycl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ MyGank-ios/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  MyGank-ios
+//
+//  Created by ycl on 2017/9/4.
+//  Copyright © 2017年 ycl. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
